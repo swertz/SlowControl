ece86d8fa8943f6b76514c0f8a5ac2744783dfa0
A.De <52782167+ADebla@users.noreply.github.com>
2020-04-01 11:02:04 +0200

    Update SlowControl.h

--- SlowControl.h
+++ SlowControl.h
@@ -41,142 +41,142 @@
 
 class SlowControl {
 	public :
-		/**
+        /**
          *  Constructor.
          */
         SlowControl();
-		
-		/**
+        
+        /**
          * Initialises the Slow Control Library
          */
-		void begin(const char* mqtt_server = SLOWCONTROL_DEFAULT_MQTT_SERVER); //--> Verified
-		
-		/**
+        void begin(const char* mqtt_server = SLOWCONTROL_DEFAULT_MQTT_SERVER); //--> Verified
+        
+        /**
          * Start running of the Slow Control Library
          */
-		void run(bool statusReadTempDS,bool statusReadTempSHT, bool statusReadHumiditySHT, bool statusCalculateDewPointSHT, bool publishToMQTT); //--> Verified	
+        void run(bool statusReadTempDS,bool statusReadTempSHT, bool statusReadHumiditySHT, bool statusCalculateDewPointSHT, bool publishToMQTT); //--> Verified	
 
-		/**
+        /**
          * Initialises the WIFI Communication
          *
          * Take previous credentials if the board has already connected to Wifi
-		 *
-		 *If not create an Wifi AP to enter the credentials of our personal Network
+        *
+         *If not create an Wifi AP to enter the credentials of our personal Network
          */
-		void connectToWifi(); //--> Verified
-		
-		/**
+        void connectToWifi(); //--> Verified
+        
+        /**
          * Reset WiFi Settings
          *
          * In order to change WiFi Credentials, you can call this function to perform a reset of them
          *
-		 */
-		void resetWifiSettings(); //--> Verified
-		
-		/**
+         */
+        void resetWifiSettings(); //--> Verified
+        
+        /**
          * Initialises the MQTT Communication
          *
          *Take default MQTT Server, Client ID & TTL Subscription feedback
-		 *
-		 *If it doesnt suit, pass yours as paramaters
+         *
+         *If it doesnt suit, pass yours as paramaters
          */
-		void connectToMQTT(int nbr=SLOWCONTROL_DEFAULT_NBR_OF_TRY, const char* clientID = SLOWCONTROL_DEFAULT_MQTT_CLIENT_ID,bool connectToTTL=false); //--> Verified
-		
-		/**
+        void connectToMQTT(int nbr=SLOWCONTROL_DEFAULT_NBR_OF_TRY, const char* clientID = SLOWCONTROL_DEFAULT_MQTT_CLIENT_ID,bool connectToTTL=false); //--> Verified
+        
+        /**
          * Publish to MQTT Topic
          *
          *Param 1 : Topic where you want to publish || Param 2 : Data you want to publish
-		 *
+         *
          */
-		void publishToMQTT(const char* topic, const char* payload);	//--> Verified
-			
-		/**
+        void publishToMQTT(const char* topic, const char* payload);	//--> Verified
+        
+        /**
          * Subscribe to TTL Status
          *
          *Take default TTL Topic as paramaters
-		 *
-		 *If it doesnt suit, pass yours as paramaters
-         */
-		void subscribeToSCBStatus(const char* topic=statusTTL_topic); //--> Verified
-			
-		/**
+        *
+        *If it doesnt suit, pass yours as paramaters
+        */
+        void subscribeToSCBStatus(const char* topic=statusTTL_topic); //--> Verified
+        
+        /**
          * Gets number of Temperature Sensors.
-		 *
+        *
          * Print on the serial monitor value indicating the number of DS sensors that are on the OneWire BUS.
          */
         void getNumberOfTemperatureSensors(); //--> Verified
-		
-		/**
+        
+        /**
          * Gets number of Humidity Sensors.
          *
          * Print on the serial monitor value indicating the number of SHT sensors that are on the OneWire BUS.
          */
         void getNumberOfHumiditySensors(); //--> Verified
-		
-		/**
+        
+        /**
          * Gets a single temperature reading from the sensor.
          *
          * Write value to global variable indicating the temperature of DS Sensor.
          */
         void readTemperature();
-		
-		/**
+        
+        /**
          * Gets a single relative humidity reading from the sensor.
          *
          * Write value to global variable indicating the temperature of SHT Sensor.
          */
         void readTempSHT();
-		
-		/**
+        
+        /**
          * Gets a single relative humidity reading from the sensor.
          *
          * Write value to global variable indicating the Humidity of SHT Sensor.
          */
-		void readHumiditySHT();
-		
-		/**
+        void readHumiditySHT();
+        
+        /**
          * Calculate dewPoint from Temperature & Humidity.
          *
          * Calculate from SHTTemp & SHTHumi and write value to global variable indicating the DewPoint .
          */
         void calculateDewPointSHT();
 
-		 /**
+         /**
          * Setup callback function 
          *
-		 * Get feeback from topic that changed if you subscribed to them
+         * Get feeback from topic that changed if you subscribed to them
          */
-		void callbackTTL(char* topic, byte* payload, unsigned int length); 		
-		
-	private :	
-		/**
+        void callbackTTL(char* topic, byte* payload, unsigned int length); 		
+        
+	private :
+        /**
          *  Constructor.
          */
-		WiFiManager wifiManager;
-		WiFiClient espClient;
-		PubSubClient mqttClient;
-		DallasTemperature ds;
-		SHT85 sht;
-			
-		/**
+        WiFiManager wifiManager;
+        WiFiClient espClient;
+        PubSubClient mqttClient;
+        DallasTemperature ds;
+        SHT85 sht;
+        
+        /**
          *  Status of Sensors Variables.
          */
-		 bool dsFound;
-		 bool shtFound;	 
-		 int deviceTempCount;
-		 float tempDSC;
-		 float tempSHTC;
-		 float humiSHT;
-		 double dewPointSHT;
-		 const char* myClientID;
-		 bool ttlSimulate;
-		 bool ttlStatus;
-		 bool myConnectToTTL;
-		  
-		/**
+        bool dsFound;
+        bool shtFound;
+        int deviceTempCount;
+        float tempDSC;
+        float tempSHTC;
+        float humiSHT;
+        double dewPointSHT;
+        const char* myClientID;
+        bool ttlSimulate;
+        bool ttlStatus;
+        bool myConnectToTTL;
+        
+        /**
          *  Functions
          */
-		 String byteArrayToString( byte*payload, unsigned int length);
+        String byteArrayToString( byte*payload, unsigned int length);
 
 };
 
